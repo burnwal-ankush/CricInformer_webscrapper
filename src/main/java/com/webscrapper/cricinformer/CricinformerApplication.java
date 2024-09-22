0596d073715089fa0c91aa35b31f9fe72b7a0944
ankush.burnwal <ankush.burnwal@subex.com>
2024-09-22 10:42:57 +0530

    Initial commit

---
+++ src/main/java/com/webscrapper/cricinformer/CricinformerApplication.java
@@ -0,0 +1,13 @@
+package com.webscrapper.cricinformer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricinformerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricinformerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webscrapper/cricinformer/CricinformerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webscrapper.cricinformer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricinformerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
